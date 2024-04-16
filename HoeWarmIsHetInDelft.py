a004984508b319ac0677fd10171fdc86b74471ad
Ahmed Mamdouh <ahmed.ussef248@gmail.com>
2024-04-16 17:29:47 +0000

    Update HoeWarmIsHetInDelft.py

--- HoeWarmIsHetInDelft.py
+++ HoeWarmIsHetInDelft.py
@@ -4,16 +4,17 @@ from utility import *
 URL = 'https://weerindelft.nl/clientraw.txt'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
 
-def get_temperature() -> float:
-    # sometime balance loaders and firewalls block python as a user-agent
+def get_temperature() -> int:
+    # sometime balance-loaders and firewalls block python as a user-agent
     headers = {"User-agent": USER_AGENT}
     # simulate the interactive frontend behavior
-    try: response = get(URL + '?' + str(now), headers = headers).text
-    except ConnectionError: 
+    try: response = get(URL + '?' + str(now()),
+                        headers = headers).text
+    except ConnectionError:
         fault("(!) Failed Connecting to weerindelft.nl Service")
         return None
-    try: return float(response.split(' ')[46])
-    except (ValueError, IndexError): 
+    try: return round(float(response.split(' ')[46]))
+    except (ValueError, IndexError):
         fault("(!) Failed to Parse Respone:")
         fault("-----"*2 + "BEGIN RESPONE" + "-----"*2)
         fault(response)
