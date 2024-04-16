b9b59e52d53266b177d3cd5beef18a927dc72bcd
Ahmed Mamdouh <ahmed.ussef248@gmail.com>
2024-04-16 22:33:18 +0000

    Update 2 files
    - /utility.py
    - /HoeWarmIsHetInDelft.py

--- HoeWarmIsHetInDelft.py
+++ HoeWarmIsHetInDelft.py
@@ -1,5 +1,21 @@
 from requests import get , ConnectionError
-from utility import *
+from sys import stderr
+from time import time
+
+# on my analysis of the webpage functionality
+# I found that ajaxWDxy.js constantly requests and update the UI
+# from the following path https://weerindelft.nl/clientraw.txt
+# followed by the JavaScript function output Date.getTime()
+# EXAMPLE: https://weerindelft.nl/clientraw.txt?1713279404355
+# which is a text file that is easily parsed
+
+# just to log errors if needed
+def fault(*args, **kwargs) -> None:
+    return print(*args, file=stderr, **kwargs)
+
+# simulates Date().getTime in Javascript
+def now() -> int: return int(time() * 1000)
+
 
 URL = 'https://weerindelft.nl/clientraw.txt'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
@@ -8,10 +24,12 @@ def get_temperature() -> int:
     # sometime balance-loaders and firewalls block python as a user-agent
     headers = {"User-agent": USER_AGENT}
     # simulate the interactive frontend behavior
+    
     try: response = get(URL + '?' + str(now()), headers = headers).text
     except ConnectionError:
         fault("(!) Failed Connecting to weerindelft.nl Service")
         return None
+    
     # attempt to parse the response
     try: return round(float(response.split(' ')[46]))
     except (ValueError, IndexError):
