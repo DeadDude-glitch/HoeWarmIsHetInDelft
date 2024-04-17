113ab01e8e2c714f5d85e71a8de93a36915e3a87
Ahmed Mamdouh <ahmed.ussef248@gmail.com>
2024-04-17 12:49:34 +0000

    Better Code

--- dockerfile
+++ dockerfile
@@ -1,10 +1,12 @@
 FROM python:3.11.8
-
 LABEL Name = "Weather in Delft App"
-
 WORKDIR /app
-ENV PYTHONUNBUFFERED=1
 
+# setting up python
+ENV PYTHONUNBUFFERED=1
+RUN pip install --no-cache-dir --upgrade pip
 RUN pip install requests
+
+# setting up application 
 COPY ./src/ ./
 CMD [ "python", "./HoeWarmIsHetInDelft.py" ]
--- src/HoeWarmIsHetInDelft.py
+++ src/HoeWarmIsHetInDelft.py
@@ -2,36 +2,39 @@ from requests import get , ConnectionError
 from sys import stderr
 from time import time
 
+"""
 # on my analysis of the webpage functionality
 # I found that ajaxWDxy.js is used to update the UI
 # from the following path https://weerindelft.nl/clientraw.txt
 # followed by the JavaScript function output Date.getTime()
 # EXAMPLE: GET https://weerindelft.nl/clientraw.txt?1713279404355
 # which is a text file that is easily parsed
+"""
 
 def fault(*args, **kwargs) -> None:
 # output to standard character error
     return print(*args, file=stderr, **kwargs)
 
-# simulates Date().getTime in Javascript
-def now() -> int: return int(time() * 1000)
+def now() -> int: 
+# simulates Date().getTim() in Javascript
+    return int(time() * 1000)
 
 
-URL = 'https://weerindelft.nl/clientraw.txt'
+URL = 'https://weerindelft.nl/clientraw.txt?'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
 
-def get_temperature() -> int:
+def get_temperature() -> float:
     # sometime balance-loaders and firewalls block python as a user-agent
-    headers = {"User-agent": USER_AGENT}
-    # simulate the interactive frontend behavior
+    # simulate the interactive frontend behavior and avoid triggering detection rules
     
-    try: response = get(URL + '?' + str(now()), headers = headers).text
+    headers = {"User-agent": USER_AGENT}
+    try: response = get(URL + str(now()), headers = headers).text
     except ConnectionError:
         fault("(!) Failed Connecting to weerindelft.nl Service")
         return None
     
     # attempt to parse the response
-    try: return round(float(response.split()[4]))
+    try: return float(response.split()[4])
     except (ValueError, IndexError):
         fault("(!) Failed to Parse Respone:")
         fault("-----"*2 + "BEGIN RESPONE" + "-----"*2)
@@ -41,6 +44,6 @@ def get_temperature() -> int:
 
 
 if __name__ == '__main__':
-    temp = get_temperature()
+    temp = round(get_temperature())
     if temp == None : exit(1)
     print(temp,'degrees Celsius')
